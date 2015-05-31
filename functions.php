dd68e7a2c640335cd2ea63ee10b9031cf27bd76d
wormeyman <wormeyman@gmail.com>
2015-05-31 16:59:37 -0700

    Revert "Allow SVG Uploads"
    This reverts commit 130aa747e7ceb6f07ec6a54f779838bdbc252635.

--- functions.php
+++ functions.php
@@ -12,11 +12,3 @@ add_action( 'wp_enqueue_scripts', 'puresimple_child_enqueue_scripts_styles' );
 * Add your own functions to this file. You can also copy some of the theme functions into this file.
 * Wordpress will use those functions instead of the original functions then.
 */
-
-/*Allow SVG Uploads*/
-function cc_mime_types($mimes) {
-  $mimes['svg'] = 'image/svg+xml';
-  return $mimes;
-}
-add_filter('upload_mimes', 'cc_mime_types');
-/*End Allow SVG Uploads*/
