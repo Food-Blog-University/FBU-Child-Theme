2a1c7c700b9efa725f432c307f9c7425bc5b79fb
wormeyman <wormeyman@gmail.com>
2015-07-02 07:59:31 -0700

    add FB conversion Code

--- header.php
+++ header.php
@@ -19,6 +19,9 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+
+<!-- Facebook Conversion Code for Eventbrite clicks - Diana Johnson 1 --><script>(function() {var _fbq = window._fbq || (window._fbq = []);if (!_fbq.loaded) {var fbds = document.createElement('script');fbds.async = true;fbds.src = '//connect.facebook.net/en_US/fbds.js';var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(fbds, s);_fbq.loaded = true;}})();window._fbq = window._fbq || [];window._fbq.push(['track', '6024580984808', {'value':'0.00','currency':'USD'}]);</script><noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6024580984808&amp;cd[value]=0.00&amp;cd[currency]=USD&amp;noscript=1" /></noscript>
+<!-- End FB CCFEC -->
 <?php wp_head(); ?>
 </head>
 
